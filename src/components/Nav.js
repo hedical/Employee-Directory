79323a87c053c30f39085a5d39522daf0803527e
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-08-14 02:00:35 -0700

    set up components, nav, search, table

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">Employee Directory</Link>
+        </nav>
+    )
+}
+
+export default Nav
