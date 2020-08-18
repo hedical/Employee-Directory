eb63379ee67fcc94e7b6dd22185091621d0229e3
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-08-18 11:39:58 -0700

    Updates

---
+++ precache-manifest.de0a1ec57087a618e7f8a667fc6406f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f39369ca2d00afc98bc1081ee185e10",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "7620cfd94f0832fc6940",
+    "url": "/Employee-Directory/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "392b50fa2b355f526eee",
+    "url": "/Employee-Directory/static/js/2.dbe55fb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Employee-Directory/static/js/2.dbe55fb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7620cfd94f0832fc6940",
+    "url": "/Employee-Directory/static/js/main.83ffaad8.chunk.js"
+  },
+  {
+    "revision": "1b13436491d4ecd8c222",
+    "url": "/Employee-Directory/static/js/runtime-main.39ec69b2.js"
+  }
+]);
